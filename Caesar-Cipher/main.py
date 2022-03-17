9c29b509ea193d611f799ee4106e1ccace1c47fd
Paula Mendez <paulipotter@gmail.com>
2022-03-16 20:46:04 -0700

    start day 8

---
+++ Caesar-Cipher/main.py
@@ -0,0 +1,26 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+text = input("Type your message: ").lower()
+shift = int(input("Type the shift number: "))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(plain_txt, shift_amt):
+    for letter in plain_txt:
+        plain_letter = alphabet.index(letter)
+        new_index = plain_letter + shift_amt
+        ciphertext+=alphabet[new_index]
+
+#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+#e.g. 
+#plain_text = "hello"
+#shift = 5
+#cipher_text = "mjqqt"
+#print output: "The encoded text is mjqqt"
+
+##HINT: How do you get the index of an item in a list:
+#https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
--- README.md
+++ README.md
@@ -13,8 +13,10 @@ Here's where I'll be logging the little projects I do for 100 days of Code. I'm 
 |5|Random Password Generator|Learned how to shuffle within a list|[Repo](https://github.com/paulipotter/Python-100-Days-of-Code/blob/main/Password-Generator/main.py)|
 |6|2D Maze Solver|Refresh on functions|[Reeborg's World](https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)|
 |7|Hangman!|Refresh on planning code, importing modules|[Repo](https://github.com/paulipotter/Python-100-Days-of-Code/tree/main/Hangman)|
+|8|Caesar Cipher|Learned about Positional vs. Keyword Arguments|[Repo](https://github.com/paulipotter/Python-100-Days-of-Code/tree/main/Caesar-Cipher)
+
 
 
 TO DO:
 Make this md prettier!
-
+c6p@ok1UwD$FM92P6tpu51yX
