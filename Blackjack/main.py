573560e73e6e2f0e43dba7d24134aa57ba383bd3
Paula Mendez <paulipotter@gmail.com>
2022-03-25 11:29:29 -0700

    final details

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -19,80 +19,67 @@ def ace_plus_ten(cards):
     else:
         return False
     
-def calculate_score(card_list):
+def calculate_score(cards):
     """Take a list of cards and return the score calculated from the cards"""
-    return sum(card_list)
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0 # zero = blackjack
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        return sum(cards)
+    return sum(cards)
 
-def dealer_wins():
-    print('Dealer wins! You lose :(')
+def compare(user_score, dealer_score):
+    if user_score > 21 and dealer_score > 21:
+        return "Your score is over 21. You lose!\n"
+    if user_score == dealer_score:
+        return "It's a draw!\n"
+    elif user_score == 0:
+        return "Win with a Blackjack!\n"
+    elif user_score > 21:
+        return "Your score is over 21. You lose!\n"
+    elif user_score > dealer_score:
+        return "You win!\n"
+    else: return "You lose!\n"
 
-def user_wins():
-    print('You win! Congrats!')
-
-def draw():
-    print("It's a draw!")
-  
-keep_playing = True
-while keep_playing:  
+def blackjack():   
+    #Init list and deal first card
     user_cards = [deal_card()]
     dealer_cards = [deal_card()]
 
-    keep_going = True
-    while(keep_going):
+    game_over = False
+    while not game_over:
+        #Deal second card and calculate score
         user_cards.append(deal_card())
         user_score = calculate_score(user_cards)
         dealer_cards.append(deal_card())
         dealer_score = calculate_score(dealer_cards)
         
+        #Print cards and console scores
         print(logo)
-        print(f"Your cards: {user_cards}, your score: {user_score}")
-        print(f"Computer's first card: {dealer_cards[0]}")
+        print(f"\nYour cards: {user_cards}, your score: {user_score}")
+        print(f"Computer's first card: {dealer_cards[0]}\n")
 
-        if ace_plus_ten(user_cards):
-            user_wins()
-            
-        elif ace_plus_ten(dealer_cards):
-            print(f"Computer has {dealer_cards}")
-            dealer_wins()
-            
-
-        if user_score > 21:
-            if 11 in user_cards:
-                user_cards[user_cards.index(11)] = 1
-                if calculate_score(user_cards) > 21:
-                    dealer_wins()
-                    
-                else: 
-                    print(f"Your cards: {user_cards}, your score: {user_score}")
+        #Check if any have a Blackjack or if user already lost - If so, game over
+        if user_score == 0 or dealer_score == 0 or user_score > 21:
+            game_over = True
+        else:        
+            more_cards = input('Would you like to get another card? y/n: ').lower()
+            if more_cards == 'y':
+                user_cards.append(deal_card())
             else:
-                dealer_wins()
-                
-                    
-        more_cards = input('Would you like to get another card? y/n: ').lower()
-
-        if more_cards == 'y':
-            continue
-        else:
-            keep_going = False
-            
-    dealer_cards.append(deal_card())
-    dealer_score = calculate_score(dealer_cards)
-
-    if dealer_score > 21:
-        user_wins()
+                game_over = True
+    #Dealer gets more cards until while loop breaks
+    while dealer_score != 0 and dealer_score < 17:
+        dealer_cards.append(deal_card())
+        dealer_score = calculate_score(dealer_cards)
         
-    else:
-        if user_score > dealer_score:
-            user_wins()
-        elif user_score < dealer_score:
-            dealer_wins()
-        else:
-            draw()
-    
-    keep_playing = input('Would you like to play again? y/n: ').lower()
+    #Print final game numbers
+    print(f"\nYour final cards are {user_cards}, final score: {user_score}")
+    print(f"The dealer's final cards are {dealer_cards}, final score: {dealer_score}")
+    print(compare(user_score, dealer_score))
 
-    if keep_playing == 'y':
-        clear()
-    else:
-        keep_playing = False
         
+while input("Would you like to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+  clear()
+  blackjack()
