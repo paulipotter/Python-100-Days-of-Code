b91cbc69799f33313c51032755534033e3bdf85a
Paula Mendez <paulipotter@gmail.com>
2022-03-28 14:03:31 -0700

    added functions and polished logic

--- Number-Guessing-Game/main.py
+++ Number-Guessing-Game/main.py
@@ -1,32 +1,44 @@
 from random import randint
+EASY_LEVEL = 10
+HARD_LEVEL = 5
 
-print('Welcome to the Number Guessing Game!')
-print("I'm thinking of a number between 1 and 100")
-
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-# if easy then 100, else if hard then 5
-if difficulty == 'easy':
-    attempts_left = 10
-elif difficulty == 'hard':
-    attempts_left = 5
-    
-guessed = False
-# generate number
-number = randint(1,100)
-while attempts_left > 0 and not guessed:
-    print(f"\n\nnumber is {number}")
-    print(f"You have {attempts_left} attempts remaining to guess the number.")
-    guess = int(input("Make a guess: "))
-    
-    if guess > number:
+def check_answer(user_guess, number, attempts_left):
+    if user_guess > number:
         print("Too high.\nGuess again.")
-        attempts_left -= 1
-    elif guess < number:
+        return attempts_left - 1
+    elif user_guess  < number:
         print("Too low.\nGuess again.")
-        attempts_left -= 1
+        return attempts_left - 1
     else: #guess is correct
         print(f"You guessed it! The answer is {number}")
-        guessed = True
+
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    # if easy then 100, else if hard then 5
+    if difficulty == 'easy':
+        return EASY_LEVEL
+    elif difficulty == 'hard':
+        return HARD_LEVEL
+
+def game():
+    print('Welcome to the Number Guessing Game!')
+    print("I'm thinking of a number between 1 and 100")
+
+    # number of turns depends on difficulty chosen
+    attempts_left = set_difficulty()
+        
+    # generate number
+    number = randint(1,100)
+    user_guess = 0
+    while user_guess != number:
+        print(f"\n\nYou have {attempts_left} attempts remaining to guess the number.")
+        user_guess = int(input("Make a guess: "))
+        attempts_left = check_answer(user_guess, number, attempts_left)
+        if attempts_left == 0:
+            print("You're out of guesses. Game Over.")
+            
+game()
+    
         
         
         
