9e91a398fc82a8686de5ef8ef177fa9bc2a473ae
Paula Mendez <paulipotter@gmail.com>
2022-03-28 12:28:38 -0700

    start project

---
+++ Number-Guessing-Game/main.py
@@ -0,0 +1,10 @@
+print('Welcome to the Number Guessing Game!')
+print("I'm thinking of a number between 1 and 100")
+
+input("Choose a difficulty. Type 'easy' or 'hard': ")
+# if easy then 100, else if hard then 5
+
+#loop
+print(f"You have {attempts_left} attempts remaining to guess the number.")
+input("Make a guess: ")
+
