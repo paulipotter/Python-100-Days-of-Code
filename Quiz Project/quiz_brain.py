21a9e92b7df85696972c569d0fe1026e46d60a3c
Paula Mendez <paulipotter@gmail.com>
2022-04-02 08:28:41 -0700

    finish up quiz class

---
+++ Quiz Project/quiz_brain.py
@@ -0,0 +1,24 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer is {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}\n")
+
