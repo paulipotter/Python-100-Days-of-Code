ac3df29f67992afa4f85a87ab0e12728e81c4569
Paula Mendez <paulipotter@gmail.com>
2022-04-13 22:44:00 -0700

    start ball object

---
+++ Pong/ball.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
