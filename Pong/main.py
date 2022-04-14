2e5bcfe24fe90351f2b0eb2873386416b4f0f514
Paula Mendez <paulipotter@gmail.com>
2022-04-13 22:44:22 -0700

    start pong game

---
+++ Pong/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong! - paulipotter")
+screen.tracer(0)
+
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
+game_over = False
+
+while not game_over:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+screen.exitonclick()
