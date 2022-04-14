7d12670b0e4ec5d075560fae85d76f95eac646ff
Paula Mendez <paulipotter@gmail.com>
2022-04-13 23:13:09 -0700

    bounce the ball!

--- Pong/ball.py
+++ Pong/ball.py
@@ -15,5 +15,8 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
