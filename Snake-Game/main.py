555eea0496dff4002da9e03104e2f4e9064e9046
Paula Mendez <paulipotter@gmail.com>
2022-04-18 12:15:30 -0700

    update readme to add day 24

--- Snake-Game/main.py
+++ Snake-Game/main.py
@@ -35,15 +35,17 @@ while not game_over:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_over = True
-        scoreboard.game_over()
+        scoreboard.reset()
+        time.sleep(0.5)
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
-            game_over = True
-            scoreboard.game_over()
+            scoreboard.reset()
+            time.sleep(0.5)
+            snake.reset()
 
 screen.exitonclick()
