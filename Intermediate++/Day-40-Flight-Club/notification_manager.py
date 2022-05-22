74449318af9d07714348b58351461fd779166054
Paula Mendez <paulipotter@gmail.com>
2022-05-22 14:41:32 -0700

    add day 40

---
+++ Intermediate++/Day-40-Flight-Club/notification_manager.py
@@ -0,0 +1,34 @@
+import smtplib
+from twilio.rest import Client
+
+TWILIO_SID = ''
+TWILIO_AUTH_TOKEN = ''
+TWILIO_VIRTUAL_NUMBER = ''
+TWILIO_VERIFIED_NUMBER = ''
+MAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com"
+MY_EMAIL = ''
+MY_PASSWORD = ''
+
+class NotificationManager:
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        print(message.sid)
+
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
+                )
