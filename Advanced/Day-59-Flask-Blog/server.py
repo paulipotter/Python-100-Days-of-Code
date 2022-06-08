cb4c6b6f392c297ecddc5c6521f845dcad0d8136
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-07 21:14:48 -0700

    day 59

--- Advanced/Day-59-Flask-Blog/server.py
+++ Advanced/Day-59-Flask-Blog/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from post import Post
 import requests
 
-# posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
-# post_objects = []
-# for post in posts:
-#     post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-#     post_objects.append(post_obj)
+posts = requests.get("https://api.npoint.io/55a84acc1ba7541787d6").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ app = Flask(__name__)
 def home():
     # response = requests.get('https://api.npoint.io/bc626e1a1760737d55c2')
     # all_posts = response.json()
-    return render_template("index.html")
+    return render_template("index.html", post_objects=post_objects)
 
 @app.route('/contact')
 def contact():
@@ -33,14 +34,14 @@ def post():
     # all_posts = response.json()
     return render_template("post.html")
 
-# @app.route("/post/<int:index>")
-# def show_post(index):
-#     requested_post = None
-#     for blog_post in post_objects:
-#         if blog_post.id == index:
-#             requested_post = blog_post
-#     return render_template("post.html", post=requested_post)
-#
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
