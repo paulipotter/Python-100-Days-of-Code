9360fbd3f300dbf44d608663d997ac81b7957566
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-07 19:24:38 -0700

    add py files

--- Intermediate++/Day-59-Flask-Blog/post.py
+++
@@ -1,6 +0,0 @@
-class Post:
-    def __init__(self, post_id, title, subtitle, body):
-        self.id = post_id
-        self.title = title
-        self.subtitle = subtitle
-        self.body = body
