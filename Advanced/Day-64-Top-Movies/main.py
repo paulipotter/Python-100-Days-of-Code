a9bfe54bbe90a54cc8eb209a72fc71d4f5ad30e0
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-20 14:10:15 -0700

    requirement 1: be able to view movie list items

--- Advanced/Day-64-Top-Movies/main.py
+++ Advanced/Day-64-Top-Movies/main.py
@@ -8,12 +8,42 @@ import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///my-top-movies.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
+db = SQLAlchemy(app)
 
+
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(300), unique=False, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(200), unique=False, nullable=False)
+    img_url = db.Column(db.String(300), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Author %r>' % self.author
+
+new_movie = Movies(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+db.create_all()
+# db.session.add(new_movie)
+# db.session.commit()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies_db = db.session.query(Movies).all()
+    return render_template("index.html", all_movies_db=all_movies_db)
 
 
 if __name__ == '__main__':
