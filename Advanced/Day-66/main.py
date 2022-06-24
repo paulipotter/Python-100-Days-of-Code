81cd4091c268bef19a5396c14f1127474120b318
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-24 12:11:18 -0700

    http get all cafes

--- Advanced/Day-66/main.py
+++ Advanced/Day-66/main.py
@@ -24,6 +24,20 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+        # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
@@ -38,25 +52,17 @@ def home():
 
 ## HTTP DELETE - Delete Record
 
+@app.route("/all")
+def all():
+    cafes = db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
 
 @app.route("/random", methods=["GET"])
 def random():
     cafes = db.session.query(Cafe).all()
     random_cafe = choice(cafes)
-    print(random_cafe)
-    return jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price,
-    })
+    return jsonify(cafe=random_cafe.to_dict())
 
 
 if __name__ == '__main__':
