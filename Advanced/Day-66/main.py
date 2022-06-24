efe56a40cf2c18be0229ce5634e65d977db38d1a
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-24 15:04:56 -0700

    http patch cafe

--- Advanced/Day-66/main.py
+++ Advanced/Day-66/main.py
@@ -4,13 +4,13 @@ from random import choice
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -35,8 +35,8 @@ class Cafe(db.Model):
             dictionary[column.name] = getattr(self, column.name)
         return dictionary
 
-        # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        # # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
 @app.route("/")
@@ -44,13 +44,6 @@ def home():
     return render_template("index.html")
     
 
-## HTTP GET - Read Record
-
-## HTTP POST - Create Record
-
-## HTTP PUT/PATCH - Update Record
-
-## HTTP DELETE - Delete Record
 @app.route("/add", methods=["POST"])
 def add_cafe():
     new_cafe = Cafe(
@@ -70,21 +63,33 @@ def add_cafe():
     return jsonify(response={"success": "Successfully added the new cafe."})
 
 
+@app.route("/update-price/<cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+
+
 @app.route("/all", methods=["GET"])
-def all():
+def get_all_cafes():
     cafes = db.session.query(Cafe).all()
     return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
 
 
 @app.route("/random", methods=["GET"])
-def random():
+def get_random_cafe():
     cafes = db.session.query(Cafe).all()
     random_cafe = choice(cafes)
     return jsonify(cafe=random_cafe.to_dict())
 
 
 @app.route("/search", methods=["GET"])
-def search():
+def search_cafe():
     query_location = request.args.get("loc")
     cafe = db.session.query(Cafe).filter_by(location=query_location).first()
     if cafe:
