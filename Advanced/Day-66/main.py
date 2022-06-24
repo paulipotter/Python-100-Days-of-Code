88480ee7fa3c92507567bf2331a859c376e0edaf
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-24 12:00:05 -0700

    http get a random cafe

--- Advanced/Day-66/main.py
+++ Advanced/Day-66/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from random import choice
 
 app = Flask(__name__)
 
@@ -36,6 +37,26 @@ def home():
 ## HTTP PUT/PATCH - Update Record
 
 ## HTTP DELETE - Delete Record
+
+
+@app.route("/random", methods=["GET"])
+def random():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = choice(cafes)
+    print(random_cafe)
+    return jsonify(cafe={
+        "id": random_cafe.id,
+        "name": random_cafe.name,
+        "map_url": random_cafe.map_url,
+        "img_url": random_cafe.img_url,
+        "location": random_cafe.location,
+        "seats": random_cafe.seats,
+        "has_toilet": random_cafe.has_toilet,
+        "has_wifi": random_cafe.has_wifi,
+        "has_sockets": random_cafe.has_sockets,
+        "can_take_calls": random_cafe.can_take_calls,
+        "coffee_price": random_cafe.coffee_price,
+    })
 
 
 if __name__ == '__main__':
