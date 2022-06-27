29b8b5d3af84a5d213b52e8ef907be4cd17ef41f
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-27 12:46:05 -0700

    Requirement 1 - Be Able to GET Blog Post Items

--- Advanced/Day-67-Blog/main.py
+++ Advanced/Day-67-Blog/main.py
@@ -7,21 +7,22 @@ from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 
 
-## Delete this code:
+#  Delete this code:
 # import requests
 # posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-##CONNECT TO DB
+# CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-##CONFIGURE TABLE
+# CONFIGURE TABLE
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -32,7 +33,7 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-##WTForm
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
@@ -44,14 +45,17 @@ class CreatePostForm(FlaskForm):
 
 @app.route('/')
 def get_all_posts():
+    posts = db.session.query(BlogPost).all()
     return render_template("index.html", all_posts=posts)
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
+    posts = db.session.query(BlogPost).all()
     requested_post = None
     for blog_post in posts:
-        if blog_post["id"] == index:
+        blog_id = blog_post.id
+        if blog_id == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
@@ -64,6 +68,12 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/edit_post")
+def edit_post():
+    pass
+    # return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
