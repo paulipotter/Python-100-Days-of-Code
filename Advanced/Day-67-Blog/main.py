544157ffdb9613e8abf6aca29f83651e310618e7
Paula Mendez <dev.paulamendez@gmail.com>
2022-06-27 13:34:10 -0700

    Requirement 3 - Be Able to Edit Existing Blog Posts

--- Advanced/Day-67-Blog/main.py
+++ Advanced/Day-67-Blog/main.py
@@ -73,10 +73,25 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/edit-post")
-def edit_post():
-    pass
-    # return render_template("contact.html")
+@app.route("/edit-post/<post_id>", methods=["GET"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
 @app.route("/new-post", methods=["GET", "POST"])
