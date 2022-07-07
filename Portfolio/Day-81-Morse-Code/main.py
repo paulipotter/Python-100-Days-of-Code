a251f2bef31a121703981f0614c9ec344b3bc658
Paula Mendez <dev.paulamendez@gmail.com>
2022-07-06 19:04:46 -0700

    english to morse

---
+++ Portfolio/Day-81-Morse-Code/main.py
@@ -0,0 +1,33 @@
+ENGLISH_TO_MORSE = {'A': '.-', 'B': '-...',
+                    'C': '-.-.', 'D': '-..', 'E': '.',
+                    'F': '..-.', 'G': '--.', 'H': '....',
+                    'I': '..', 'J': '.---', 'K': '-.-',
+                    'L': '.-..', 'M': '--', 'N': '-.',
+                    'O': '---', 'P': '.--.', 'Q': '--.-',
+                    'R': '.-.', 'S': '...', 'T': '-',
+                    'U': '..-', 'V': '...-', 'W': '.--',
+                    'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                    '1': '.----', '2': '..---', '3': '...--',
+                    '4': '....-', '5': '.....', '6': '-....',
+                    '7': '--...', '8': '---..', '9': '----.',
+                    '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                    '?': '..--..', '/': '-..-.', '-': '-....-',
+                    '(': '-.--.', ')': '-.--.-'}
+
+
+def english_to_morse(message):
+    print(f'upper is {message}')
+
+    morse = ""
+    for letter in message:
+        if ENGLISH_TO_MORSE.get(letter) is not None:
+            morse += ENGLISH_TO_MORSE[letter]
+        elif letter == ' ':
+            morse += ' '
+        else:
+            morse += letter
+    print(morse)
+
+
+msg = input("what message do you want to encrypt? ").upper()
+english_to_morse(msg)
